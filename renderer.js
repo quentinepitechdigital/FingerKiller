16ff0603fe633456824d3f53cfc8171c0998d8fd
quentinepitechdigital <quentin.taverne@epitech.digital>
2022-12-18 13:17:33 +0100

    Added key1 & key2 inputs

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 document.addEventListener("keydown", function (ev) {
   console.log(ev.key);
 });
+
+for (const id of ["key1", "key2"]) {
+  document.getElementById(id).addEventListener("keydown", function (ev) {
+    ev.preventDefault();
+    document.getElementById(id).value = ev.key;
+  });
+}
